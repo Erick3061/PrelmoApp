c1f78d8a4dfecc466e8e3acb9b6a56bb9bd524f3
Erick Andrade Ramos <grillo.erick1@gmail.com>
2025-07-16 12:56:43 -0600

    update Loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,55 +1,31 @@
-import React, { useEffect } from 'react';
-import { ActivityIndicator, Platform } from 'react-native';
-import Animated, { Extrapolation, FadeIn, FadeOut, interpolate, interpolateColor, SlideInUp, SlideOutUp, useAnimatedStyle, useSharedValue, withRepeat, withSequence, withSpring, withTiming } from 'react-native-reanimated';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React, { useCallback, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native-paper';
+import Animated, { Extrapolation, FadeIn, FadeOut, interpolate, interpolateColor, SlideInUp, SlideOutUp, useAnimatedStyle, useSharedValue, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
 
-const Loading = ({ loading, refresh, right }: { loading?: boolean, refresh?: boolean, right?: number }) => {
-    const insets = useSafeAreaInsets();
+const Loading = ({ loading, refresh }: { loading?: boolean, refresh?: boolean }) => {
     const offset = useSharedValue(0);
-    const offset2 = useSharedValue(0);
-    const translate: number = 20;
-    const duration: number = 800;
+    const translate: number = .1;
+    const duration: number = 600;
 
-    const Animate = () => {
-        offset.value =
-            withRepeat(
-                withSequence(
-                    withTiming(-translate, { duration }),
-                    withTiming(translate, { duration }),
-                )
-                , 1000, true);
-        offset2.value =
-            withRepeat(
-                withSequence(
-                    withTiming(translate, { duration }),
-                    withTiming(-translate, { duration }),
-                )
-                , 1000, true);
-    }
-
-    const animatedStyles = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { translateX: withSpring(offset.value) },
-            ],
-            backgroundColor: interpolateColor(offset.value, [-translate, 0, translate], ['red', 'green', 'pink'])
-        };
-    });
+    const Animate = useCallback(
+        () => {
+            offset.value = withRepeat(withSequence(withTiming(-translate, { duration }), withTiming(translate, { duration })), 1000, true)
+        },
+        [offset],
+    )
 
     const animatedStyles2 = useAnimatedStyle(() => {
         return {
             transform: [
-                { translateX: withSpring(offset2.value) },
-                { scale: interpolate(offset2.value, [-translate, translate], [1.5, 1], { extrapolateRight: Extrapolation.CLAMP }) },
+                { scale: interpolate(offset.value, [-translate, translate], [0, 1], { extrapolateRight: Extrapolation.CLAMP }) },
             ],
-            opacity: interpolate(offset.value, [-translate, 0, translate], [.8, .5, .8], { extrapolateRight: Extrapolation.CLAMP }),
-            backgroundColor: interpolateColor(offset.value, [-translate, 0, translate], ['red', 'green', 'pink'])
+            backgroundColor: interpolateColor(offset.value, [-translate, 0, translate], ['rgb(234, 221, 255)', 'rgb(103, 80, 164)', 'rgb(234, 221, 255)'])
         };
     });
 
     useEffect(() => {
         Animate();
-    }, []);
+    }, [Animate]);
 
     return (
         loading ?
@@ -59,33 +35,16 @@ const Loading = ({ loading, refresh, right }: { loading?: boolean, refresh?: boo
             ]}>
                 <Animated.View
                     style={[
-                        { width: 20, height: 20, position: 'absolute', borderRadius: 10 },
-                        animatedStyles
-                    ]}
-                />
-                <Animated.View
-                    style={[
-                        { width: 20, height: 20, backgroundColor: 'red', position: 'absolute', borderRadius: 10 },
+                        { width: 25, height: 25, position: 'absolute', borderRadius: 100 },
                         animatedStyles2,
                     ]}
-                />
-                {/* <Text>Cargando...</Text> */}
+                /><ActivityIndicator />
             </Animated.View>
             :
             refresh
                 ?
-                <Animated.View entering={SlideInUp} exiting={SlideOutUp} style={[
-                    {
-                        alignSelf: 'center',
-                        position: 'absolute',
-                        top: (insets && Platform.OS === 'ios') ? insets.top : 8,
-                        padding: 7,
-                        right,
-                        borderRadius: 100
-                    },
-                    { elevation: 5 }
-                ]}>
-                    <ActivityIndicator size={25} />
+                <Animated.View entering={SlideInUp} exiting={SlideOutUp} style={[{ alignSelf: 'center', position: 'absolute', top: 10 }]}>
+                    <ActivityIndicator />
                 </Animated.View>
                 :
                 null
